64693cec5391f8c30987af8df40e057c040c0f41
LeSe0 <levonstepanyan.ait@gmail.com>
2022-05-04 17:34:36 +0400

    feat(app) : add promise all for save data

--- src/helpers/saveData.js
+++ src/helpers/saveData.js
@@ -2,24 +2,44 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { fetchData } from "../redux/slices/fetchingData/actionCreators";
 
+const replaceShopItems = (el, dbItems) => {
+  return dbItems.forEach(elem => {
+        if(elem.id == el.id && elem.itemsLeft != el.itemsLeft){
+          return axios.put("http://localhost:8000/shopItems/" + el.id, { ...el })  
+        }
+      })
+}
+
+const replaceMyInfoItems = (el, dbItems) => {
+  return dbItems.forEach(elem => {
+          if(elem.id == el.id){
+            if(el.count > 0){
+              axios.put("http://localhost:8000/myInfo/" + el.id, el)
+            }
+            else{
+              axios.post("http://localhost:8000/myInfo" , el)
+            }
+          }
+        })
+}
+
 export const saveData = (myInfo, shopItems, pages) => (dispatch) => {
-  shopItems.forEach((el, i) => {
-    axios
-      .patch("http://localhost:8000/shopItems/" + el.id, { ...el })
-      .then(() => {
-        console.log(myInfo[i] && el.count > 0);
-        axios
-          .get("http://localhost:8000/myInfo")
-          .then((res) => {
-            return res.data;
-          })
-          .then((res) => {
-            res[i]?.id == myInfo[i]?.id
-              ? axios.patch("http://localhost:8000/myInfo/" + el.id, {
-                  ...myInfo[i],
-                })
-              : axios.post("http://localhost:8000/myInfo", myInfo[i]);
-          });
-      });
-  });
+
+  const dbShopItems = axios.get('http://localhost:8000/shopItems').then(res => res.data)
+  const dbMyInfo = axios.get('http://localhost:8000/myInfo').then(res => res.data)
+
+  console.log(dbShopItems);
+
+  const request1 = shopItems.map(el => {
+    return replaceShopItems(el, dbShopItems)
+  })
+  console.log(request1);
+
+  const request2 = myInfo.map(el => {
+    return replaceMyInfoItems(el, dbMyInfo)
+  })
+
+  Promise.all([...request1, ...request2]).then(el => {
+    console.log(el);
+  })
 };
